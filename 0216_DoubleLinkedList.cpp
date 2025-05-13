79ab70fc0f6834f60d9cf6afaca5679f23e1b634
faiqzzz <ridhofaiqzzz@gmail.com>
2025-05-13 10:45:56 +0700

    set nilai next node baru

--- 0216_DoubleLinkedList.cpp
+++ 0216_DoubleLinkedList.cpp
@@ -66,6 +66,10 @@ void addNode ()
             current = current->next; // step 1.e : move the current to the next             
         }
     
+    // set nilai next node baru = current dan prev node baru == previous
+    newNode->next = current;    // step 4: Make the next field of the new node
+    newNode->prev = previous;   // step 5: Make the previous field of the new node    
+    
     
     }
 
