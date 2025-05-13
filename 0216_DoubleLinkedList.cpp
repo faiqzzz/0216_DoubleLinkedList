0dd314bc2762f146ec94735701e42c9a44f4a190
faiqzzz <ridhofaiqzzz@gmail.com>
2025-05-13 12:10:28 +0700

    prosedur untuk mencari data dan menampilkan data yang di cari jika ada

--- 0216_DoubleLinkedList.cpp
+++ 0216_DoubleLinkedList.cpp
@@ -201,4 +201,25 @@ void revtraverse()
     }
 }
 
-    
+    // prosedur untuk mencari data dan menampilkan data yang di cari jika ada
+    void searchData()
+    {
+    if (listEmpty() == true)
+    {
+        cout << "\nList is empty" << endl;
+    }
+    Node *prev, *curr;
+    prev = curr = NULL;
+    cout << "\nEnter the roll number of the student whose record you want to search :";
+    int num;
+    cin >> num;
+    if (search(num, &prev, &curr) == false)
+        cout << "\nRecord not found " << endl;
+    else
+    {
+        cout << "\nRecords found" << endl;
+        cout << "\nRoll number : " << curr->noMhs<< endl;
+        cout << "\nName : " << curr->name << endl;
+    }
+}
+
