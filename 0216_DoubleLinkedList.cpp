eeb70a925b160df76e38ea63d38cf23fd0902378
faiqzzz <ridhofaiqzzz@gmail.com>
2025-05-13 10:18:58 +0700

    membuat node baru

--- 0216_DoubleLinkedList.cpp
+++ 0216_DoubleLinkedList.cpp
@@ -21,11 +21,7 @@ void addNode ()
 
     // pembuatan node dan pemberian value untuk data noMhs dan name 
     Node *newNode = new Node(); // step 1 : Buat node baru
-    cout << "\nEnter the roll number of the student : ";
-    cin >> newNode->noMhs; // Assign value to the data field of the new node
-    cout << "\nEnter the name of the student :";
-    cin >> newNode->name; // Assign value to the data field of th
-
     
+
 
 }
